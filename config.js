bd52e3dbfe2feae44f67d8cf50a5c5897357cf9d
LiveMedia48 <manithrissur48@gmail.com>
2025-06-26 18:36:05 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  prefix: ".", // Example: .ping
+  owner: "917994368116", // നിങ്ങളുടെ ഫോൺ നമ്പർ India format-ൽ (91xxxxxxxxxx)
+  botname: "Live Media Bot", // Bot ന്റെ പേര്
+};
